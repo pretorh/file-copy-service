1c6ca5c2fa4f2e389bc5f903aaed8127dcf3b990
Hendri Pretorius <pretorh@gmail.com>
2013-10-29 21:45:52 +0200

    basic request validation

--- example/test.js
+++ example/test.js
@@ -1,1 +1,31 @@
-var copyService = new require("../").CopyService();
+var cs = require("../");
+
+var copyService = new cs.CopyService();
+
+require("fs").writeFileSync("./test.dat", "some data in the file");
+
+var id1 = copyService.copy({
+    source: "test.dat",
+    dest: "./test-copy.dat"
+});
+var id2 = copyService.copy();
+
+console.log(id1);
+console.log(copyService.status(id1));
+
+process.nextTick(function() {
+    console.log(copyService.status(id1));
+    console.log(copyService.status(id2));
+    
+    function wait() {
+        var stat = copyService.status(id1);
+        if (stat.status === "pending") {
+            setTimeout(wait, 10);
+            require("fs").unlinkSync("./test.dat");
+        } else {
+            console.log(stat);
+        }
+    }
+    
+    wait();
+});
--- src/copier.js
+++ src/copier.js
@@ -1,5 +1,103 @@
 module.exports = CopyService;
 
+var fs = require("fs");
+
 function CopyService() {
     var self = this;
+    
+    var devices = [];
+    var items = {};
+    
+    self.copy = function(request) {
+        var id = createId();
+        items[id] = new Item(request, function() {
+            
+        });
+        return id;
+    }
+    
+    self.status = function(id) {
+        return items[id].info;
+    }
+}
+
+function Item(request, readyCallback) {
+    var self = this;
+    self.info = {
+        status: "pending",
+        message: "preparing"
+    };
+    
+    self.fail = function(message, detailed) {
+        self.setStatus("failed", message);
+        if (detailed)
+            self.info.detailed = detailed;
+    }
+    
+    self.setStatus = function(status, message) {
+        self.info.status = status;
+        self.info.message = message;
+    }
+    
+    function validateRequest() {
+        if (!request || !request.source || !request.dest) {
+            self.fail("bad request");
+            return;
+        }
+        
+        fs.stat(request.source, function(err, srcStat) {
+            if (err) {
+                self.fail("source stat failed", err);
+            } else if (!srcStat.isFile()) {
+                self.fail("source is not a file");
+            } else {
+                self.info.src = {
+                    path: request.source,
+                    device: srcStat.dev,
+                    size: srcStat.size
+                };
+                process.nextTick(validateDestination);
+            }
+        });
+    }
+    
+    function validateDestination() {
+        var re = /^(.*)\/.*$/;
+        var destDir = request.dest.match(re);
+        destDir = destDir ? destDir[1] : ".";
+        
+        if (fs.existsSync(request.dest)) {
+            self.fail("destination already exist");
+            return;
+        }
+        
+        fs.stat(destDir, function(err, destStat) {
+            if (err) {
+                fail(id, "dest stat failed", err);
+            } else if (!destStat.isDirectory()) {
+                fail(id, "dest's directory stat is not a directory", destDir);
+            } else {
+                self.info.dst = {
+                    path: request.dest,
+                    device: destStat.dev
+                };
+                process.nextTick(validationDone);
+            }
+        });
+    }
+    
+    function validationDone() {
+        self.setStatus("queue", "queued on device");
+        readyCallback();
+    }
+    
+    process.nextTick(validateRequest);
+
+}
+
+function createId() {
+    // create a number 0x100000000 -> 0x1FFFFFFFF, convert to hex, drop the first digit
+    return Math.floor((1 + Math.random()) * 0x100000000)
+        .toString(16)
+        .substring(1);
 }
