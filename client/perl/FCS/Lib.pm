052698fc8483d5597b8a5c8c69daa728bbbd6650
Hendri Pretorius <pretorh@gmail.com>
2013-11-10 13:00:49 +0200

    perl client library library to copy/move files and get status details for a request

---
+++ client/perl/FCS/Lib.pm
@@ -0,0 +1,46 @@
+package FCS::Lib;
+use strict;
+use warnings;
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(copyFile status);
+
+sub request {
+    my ($request) = @_;
+    my $agent = LWP::UserAgent->new;
+    my $response = $agent->request($request);
+    if ($response->is_success) {
+        my $json = JSON->new->allow_nonref->decode($response->decoded_content);
+        return $json;
+    } else {
+        print $response->code, "\n";
+        print $response->message, "\n";
+        die("request failed");
+    }
+}
+
+sub copyFile {
+    shift @_;
+    my ($url, $fromDir, $toDir, $file, $move) = @_;
+
+    my $fullFrom = $fromDir . "/" . $file;
+    my $fullTo = $toDir . "/" . $file;
+    my $doMove = $move ? "true" : "false";
+    my $data = '{"source": "' . $fullFrom . '", "dest": "' . $fullTo . '", "move": ' . $doMove . '}';
+
+    my $request = HTTP::Request->new(POST => $url . "/copy");
+    $request->header("content-type", "application/json");
+    $request->content($data);
+    return request($request);
+}
+
+sub status {
+    shift @_;
+    my ($url, $id) = @_;
+
+    my $request = HTTP::Request->new(GET => $url . "/status/" . $id);
+    $request->header("content-type", "application/json");
+    return request($request);
+}
+
+1;
